7c2c6cf5000fe59e8d697e7457d3df8ea2cc3766
Debayan <ddebayan29@gmail.com>
2026-01-05 13:42:24 +0530

    zod fixes

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,6 +1,6 @@
 import {z} from 'zod'
 
 export const signInSchema = z.object({
-    identifier: z.string(),
+    identifier: z.string(), // identifier is industry term for email here
     password: z.string(),
 })
